09597248135eb42091490a1e52d304fcfd7ea967
William Poston <willpatpost@gmail.com>
2026-03-20 12:25:17 -0400

    reset main

--- README.md
+++
@@ -1,2 +0,0 @@
-# Crossworder
-The official testing code repository for Crossworder's static GitHub website!
--- main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-import { CrosswordSolver } from './CrosswordSolver.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    // The entry point simply boots the Coordinator
-    const app = new CrosswordSolver();
-    app.init();
-});
